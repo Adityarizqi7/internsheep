4facd2865d7714e29ef5f168bdcc24b13acf0d29
diofarrel <diofarrel.work@gmail.com>
2022-12-27 14:00:02 +0700

    Clean up index.php

--- application/views/student/index.php
+++ application/views/student/index.php
@@ -1,9 +1,9 @@
 <title>Internsheep</title>
-<link rel="stylesheet" href="<?= base_url();?>assets/css/index.css" />
+<link rel="stylesheet" href="<?= base_url(); ?>assets/css/index.css" />
 
 <main class="px-4">
-  <div class="headline mx-auto text-center">
-    <h1 class="display-6 fw-semibold montserrat">Daftarkan pengajuan magangmu <br/> sekarang juga!</h1>
+  <div class="headline mx-auto text-center" style="margin-bottom: 10rem;">
+    <h1 class="display-4 fw-bold montserrat">Daftarkan pengajuan magangmu <br /> sekarang juga!</h1>
     <p class="mt-4 montserrat">Kami menyediakan fitur untuk pencatatan mahasiswa yang akan atau sedang <br /> melakukan program magang atau praktik kerja lainnya</p>
     <div class="d-flex flex-sm-row flex-column justify-content-center montserrat">
       <button type="button" class="me-sm-4 mb-sm-0 mb-3 button-primary text-white bg-orange-base">Ajukan Laporan</button>
@@ -14,29 +14,32 @@
       </button>
     </div>
   </div>
-  <div class="content d-flex align-items-center montserrat">
-    <div class="right-content">
-      <h2 class="fs-4 fw-bolder">Tata cara pengajuan</h2>
-      <p>Simak langkah-langkah berikut untuk mengetahui cara melakukan pengajuan</p>
-      <nav class="mt-5">
-        <ul>
-          <li class="mb-5">
-            <h3 class="fs-5 fw-bold">Siapkan semua berkas-berkas yang diperlukan</h3>
-            <p>Berkas yang diperlukan yaitu meliputi surat pengajuan dan surat penerimaan dari perusahaan tempat berlangsungnya magang atau praktik kerja</p>
-          </li>
-          <li class="mb-5">
-            <h3 class="fs-5 fw-bold">Buka halaman pengajuan</h3>
-            <p>Silahkan buka halaman pengajuan pada navigation bar atau klik disini</p>
-          </li>
-          <li class="mb-5">
-            <h3 class="fs-5 fw-bold">Isikan semua form dengan benar</h3>
-            <p>Saat melakukan pengisian pastikan data diri dan berkas yang diisikan pada formulir sudah lengkap dan benar. Apabila sudah terisi semua silahka tekan tombol submit</p>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    <div class="left-content ms-5 d-lg-block d-none">
-      <img src="assets/image/user-guide-illustration.png" />
+
+  <div class="container montserrat">
+    <div class="row align-items-center">
+      <div class="col">
+        <h2 class="fs-4 fw-bolder">Tata cara pengajuan</h2>
+        <p>Simak langkah-langkah berikut untuk mengetahui cara melakukan pengajuan</p>
+        <nav class="mt-5">
+          <ul>
+            <li class="mb-5">
+              <h3 class="fs-5 fw-bold">Siapkan semua berkas-berkas yang diperlukan</h3>
+              <p>Berkas yang diperlukan yaitu meliputi surat pengajuan dan surat penerimaan dari perusahaan tempat berlangsungnya magang atau praktik kerja</p>
+            </li>
+            <li class="mb-5">
+              <h3 class="fs-5 fw-bold">Buka halaman pengajuan</h3>
+              <p>Silahkan buka halaman pengajuan pada navigation bar atau klik disini</p>
+            </li>
+            <li class="mb-5">
+              <h3 class="fs-5 fw-bold">Isikan semua form dengan benar</h3>
+              <p>Saat melakukan pengisian pastikan data diri dan berkas yang diisikan pada formulir sudah lengkap dan benar. Apabila sudah terisi semua silahka tekan tombol submit</p>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="col">
+        <img src="assets/image/user-guide-illustration.png" />
+      </div>
     </div>
   </div>
 </main>
